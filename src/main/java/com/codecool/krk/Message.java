0f429835ba4869e95d2173a52b7c1a93dbfeb217
Przemo8922 <przemyslawsalak@gmail.com>
2018-03-12 21:55:57 +0100

    Add Message classz

---
+++ src/main/java/com/codecool/krk/Message.java
@@ -0,0 +1,17 @@
+package com.codecool.krk;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class Message implements Serializable {
+    
+    private String content;
+    private String author;
+    private LocalDateTime createdAt;
+
+    public Message(String content, String author) {
+        this.content = content;
+        this.author = author;
+        this.createdAt = LocalDateTime.now();
+    }
+}
