a35df9bf972b1b9ba329595c58956a4447fc044d
Przemo8922 <przemyslawsalak@gmail.com>
2018-03-12 21:57:21 +0100

    add getter and setter to Message class

--- src/main/java/com/codecool/krk/Message.java
+++ src/main/java/com/codecool/krk/Message.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 
 public class Message implements Serializable {
-    
+
     private String content;
     private String author;
     private LocalDateTime createdAt;
@@ -14,4 +14,21 @@ public class Message implements Serializable {
         this.author = author;
         this.createdAt = LocalDateTime.now();
     }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
 }
