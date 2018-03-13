5a9019b42e18ca43334619a9f69210f94d7c4cfd
Przemo8922 <przemyslawsalak@gmail.com>
2018-03-13 13:33:50 +0100

    add method run()

--- src/main/java/com/codecool/krk/client/ClientWriteThread.java
+++ src/main/java/com/codecool/krk/client/ClientWriteThread.java
@@ -1,5 +1,8 @@
 package com.codecool.krk.client;
 
+import com.codecool.krk.message.Message;
+
+import java.io.IOException;
 import java.io.ObjectOutputStream;
 
 public class ClientWriteThread extends Thread {
@@ -10,5 +13,25 @@ public class ClientWriteThread extends Thread {
     public ClientWriteThread(ObjectOutputStream objectOutputStream, Client client) {
         this.objectOutputStream = objectOutputStream;
         this.client = client;
+    }
+
+    @Override
+    public void run() {
+        String userInput;
+        do {
+            try {
+                userInput = this.client.getStdIn().readLine();
+                if (!userInput.equalsIgnoreCase(".quit!")) {
+                    Message newMessage = new Message(userInput, this.client.getUserName());
+                    objectOutputStream.writeObject(newMessage);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        } while (!userInput.equalsIgnoreCase(".quit!"));
+        System.out.println("Bye bye!");
+
+        this.client.interruptThreads();
     }
 }
