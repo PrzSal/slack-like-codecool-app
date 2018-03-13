c5bbd793e0b954eb5bf55f179545b9c0c4932788
Przemo8922 <przemyslawsalak@gmail.com>
2018-03-13 14:47:54 +0100

    initialize SlackController in SlackLikeApp

--- src/main/java/com/codecool/krk/SlackLikeApp.java
+++ src/main/java/com/codecool/krk/SlackLikeApp.java
@@ -1,7 +1,10 @@
 package com.codecool.krk;
 
+import com.codecool.krk.appcontroller.SlackController;
+
 public class SlackLikeApp {
     public static void main( String[] args ) {
-
+        SlackController slackController = new SlackController(args);
+        slackController.startApp();
     }
 }
