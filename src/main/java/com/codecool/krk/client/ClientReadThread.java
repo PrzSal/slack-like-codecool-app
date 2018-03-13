354daa16a25561f56bf645f190415ff6ba4004ea
Przemo8922 <przemyslawsalak@gmail.com>
2018-03-13 13:28:09 +0100

    add method run()

--- src/main/java/com/codecool/krk/client/ClientReadThread.java
+++ src/main/java/com/codecool/krk/client/ClientReadThread.java
@@ -1,6 +1,10 @@
 package com.codecool.krk.client;
 
+import com.codecool.krk.message.Message;
+
+import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.net.SocketException;
 
 public class ClientReadThread extends Thread {
     private ObjectInputStream objectInputStream;
@@ -8,4 +12,23 @@ public class ClientReadThread extends Thread {
     public ClientReadThread(ObjectInputStream objectInputStream) {
         this.objectInputStream = objectInputStream;
     }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Message serverMessage = (Message) this.objectInputStream.readObject();
+                System.out.printf("%s> %s\n", serverMessage.getAuthor(), serverMessage.getContent());
+            } catch (SocketException e) {
+                break;
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+
 }
