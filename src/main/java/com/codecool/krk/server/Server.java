2ca51064b1a86c43211a8b71f1bfaf5b12b89f97
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-03-13 10:47:29 +0100

    Declare Server class and implement it constructor

---
+++ src/main/java/com/codecool/krk/server/Server.java
@@ -0,0 +1,18 @@
+package com.codecool.krk.server;
+
+import java.io.BufferedReader;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+public class Server {
+    private BufferedReader stdIn;
+    private int portNumber;
+    private HashMap<String, Thread> userThreads;
+
+    public Server(BufferedReader stdIn, int portNumber) {
+        this.stdIn = stdIn;
+        this.portNumber = portNumber;
+
+        this.userThreads = new LinkedHashMap<>();
+    }
+}
