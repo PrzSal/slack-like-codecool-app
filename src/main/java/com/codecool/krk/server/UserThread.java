3ecc3c29edda5cc4ba6e9fc87f2179e9605d7aa8
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-03-13 12:34:49 +0100

    Implement first version of run() method

--- src/main/java/com/codecool/krk/server/UserThread.java
+++ src/main/java/com/codecool/krk/server/UserThread.java
@@ -1,18 +1,49 @@
 package com.codecool.krk.server;
 
+import com.codecool.krk.message.Message;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class UserThread extends Thread {
-    private Server server;
     private Socket socket;
+    private Server server;
+    private ObjectOutputStream out;
+    ObjectInputStream in;
 
-    public UserThread(Server server, Socket socket) {
-        this.server = server;
+    public UserThread(Socket socket, Server server) {
         this.socket = socket;
+        this.server = server;
     }
 
     @Override
     public void run() {
+        try (ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+             ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
 
+            this.out = out;
+            this.in = in;
+
+            Message controlMessage = (Message) in.readObject();
+
+            if (controlMessage.getContent().equalsIgnoreCase("control")) {
+                server.addUserThread(controlMessage.getAuthor(), this);
+
+                Message clientMessage;
+                do {
+                    clientMessage = (Message) in.readObject();
+                    server.broadcastMessage(clientMessage, this);
+                } while (clientMessage.getContent().equalsIgnoreCase(".quit!"));
+
+                server.removeUser(clientMessage.getAuthor());
+            }
+            // give else with throw some error and message about lack of control message
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
