4897b0408476a590c5d4cad65fdf34c92e14cbd8
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-03-14 10:21:47 +0100

    Change call of ClientReadThread constructor, delete unused methods

--- src/main/java/com/codecool/krk/client/Client.java
+++ src/main/java/com/codecool/krk/client/Client.java
@@ -31,29 +31,13 @@ public class Client {
         return userName;
     }
 
-    public Thread getOutput() {
-        return output;
-    }
-
-    public void setOutput(Thread output) {
-        this.output = output;
-    }
-
-    public Thread getInput() {
-        return input;
-    }
-
-    public void setInput(Thread input) {
-        this.input = input;
-    }
-
     public void execute() {
         try (Socket serverSocket = new Socket(this.hostName, this.portNumber);
              ObjectOutputStream objectOutputStream = new ObjectOutputStream(serverSocket.getOutputStream());
              ObjectInputStream objectInputStream = new ObjectInputStream(serverSocket.getInputStream())
         ){
             this.output = new ClientWriteThread(objectOutputStream, this);
-            this.input = new ClientReadThread(objectInputStream);
+            this.input = new ClientReadThread(objectInputStream, this);
 
             output.start();
             input.start();
@@ -66,7 +50,6 @@ public class Client {
             System.err.println(e.getMessage());
         }
     }
-
 
     public void interruptThreads() {
         this.output.interrupt();
