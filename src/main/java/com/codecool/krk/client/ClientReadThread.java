7c1df3d32feb77fe4e0582c1076f36a2f28e517e
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-03-14 11:01:49 +0100

    Add field client, change constructor, add catch of EOFException

--- src/main/java/com/codecool/krk/client/ClientReadThread.java
+++ src/main/java/com/codecool/krk/client/ClientReadThread.java
@@ -2,25 +2,32 @@ package com.codecool.krk.client;
 
 import com.codecool.krk.message.Message;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.net.SocketException;
 
 public class ClientReadThread extends Thread {
     private ObjectInputStream objectInputStream;
+    private Client client;
 
-    public ClientReadThread(ObjectInputStream objectInputStream) {
+    public ClientReadThread(ObjectInputStream objectInputStream, Client client) {
         this.objectInputStream = objectInputStream;
+        this.client = client;
     }
 
     @Override
     public void run() {
         while (true) {
             try {
-                // throw EOFException when force disconnect server
                 Message serverMessage = (Message) this.objectInputStream.readObject();
                 System.out.printf("%s> %s\n", serverMessage.getAuthor(), serverMessage.getContent());
             } catch (SocketException e) {
+                break;
+            } catch (EOFException e) {
+                System.err.println("Server disconnected");
+                System.err.println("Hit enter to exit application");
+                this.client.interruptThreads();
                 break;
             } catch (IOException e) {
                 e.printStackTrace();
