e76c9fb72449043436120cfab09b1d1941ca4293
Marcin Ogorzalek <marcin.grzegorz.ogorzalek@gmail.com>
2018-03-14 13:13:45 +0100

    Add field room with setter and getter, refract toString() method

--- src/main/java/com/codecool/krk/message/Message.java
+++ src/main/java/com/codecool/krk/message/Message.java
@@ -4,14 +4,16 @@ import java.io.Serializable;
 import java.time.LocalDateTime;
 
 public class Message implements Serializable {
-
     private String content;
     private String author;
     private LocalDateTime createdAt;
+    private String room;
 
-    public Message(String content, String author) {
+    public Message(String content, String author, String room) {
         this.content = content;
         this.author = author;
+        this.room = room;
+
         this.createdAt = LocalDateTime.now();
     }
 
@@ -31,12 +33,17 @@ public class Message implements Serializable {
         this.author = author;
     }
 
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
     @Override
     public String toString() {
-        return "Message{" +
-                "content='" + content + '\'' +
-                ", author='" + author + '\'' +
-                ", createdAt=" + createdAt +
-                '}';
+        return String.format("Message{content='%s', author='%s', room='%s', createdAt='%s'",
+                this.content, this.author, this.room, this.createdAt);
     }
 }
